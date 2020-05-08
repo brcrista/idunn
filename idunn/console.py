6b6a371b5502329ac2e93a9b4a12d2b3db19c6c4
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2020-05-07 20:39:28 -0400

    Add colorized output

--- idunn/console.py
+++ idunn/console.py
@@ -1,14 +1,16 @@
 import sys
 
+import colorama
+
 class Console:
     def __init__(self):
-        pass
+        colorama.init()
 
     def info(self, message):
         print(f'> {message}')
 
     def warning(self, message):
-        print(f'? {message}', file=sys.stderr)
+        print(f'{colorama.Fore.YELLOW}? {message}', file=sys.stderr)
 
     def error(self, message):
-        print(f'! {message}', file=sys.stderr)
+        print(f'{colorama.Fore.RED}! {message}', file=sys.stderr)

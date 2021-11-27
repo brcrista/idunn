fc5fe3600b0c2f377f128f16854a653d67c2fddb
Brian Cristante <33549821+brcrista@users.noreply.github.com>
2021-11-26 23:29:34 -0500

    Revert "Bump to v2"
    This reverts commit 50f88e1a7bee66ba5ca873376e9213fd6665bb42.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 # Specify the version here so we can choose to obtain it from environment variables
 # later.
-VERSION = '2.0.0'
+VERSION = '1.0.0'
 
 # Specify the description here so we can use UTF-8 characters on all platforms.
 DESCRIPTION = "MusicOps, anyone?"
